a4d237202a38c222b72216b2f063329ec2d1dd39
shayleyzerovich <58327880+shayleyzerovich@users.noreply.github.com>
2021-04-28 01:47:51 +0300

    Update NumberWithUnits.cpp

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -1,258 +1,164 @@
+#include "NumberWithUnits.hpp"
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include <string>
 #include <map>
-#include <cmath>
-#include <algorithm>
-#include "NumberWithUnits.hpp"
-
 using namespace std;
-namespace ariel
-{
-    map<string, map<string, double>> units_table;
-    const double EPSILON = 0.000001;
+using namespace ariel;
 
-    NumberWithUnits::NumberWithUnits(double num, const string &unit)
-    {
-        if (units_table.count(unit) == 0)
-        {
-            throw invalid_argument{"Unit doesn't exist!!!"};
-        }
-        this->num = num;
-        this->unit = unit;
+namespace ariel{
+
+    const double eps = 0.00001;
+    static map<string, map<string, double> > units;
+
+    NumberWithUnits :: NumberWithUnits(double value, const string& type){
+        units.at(type);
+        this->value=value;
+        this->type=type;
     }
 
-    void NumberWithUnits::read_units(ifstream &units_file)
-    {
-        string line;
-        string unit1;
-        string equal;
-        string unit2;
-        double n1 = 0;
-        double n2 = 0;
-        while (units_file.good())
-        {
-            getline(units_file, line);
-            istringstream ss(line);
-            ss >> n1 >> unit1 >> equal >> n2 >> unit2;
-            units_table[unit1][unit2] = n2;
-            units_table[unit2][unit1] = 1 / n2;
-            for (const auto &element : units_table)
-            {
-                //Search unit1 in the value maps and add unit2
-                //And add the founded key to unit2's map
-                if (element.second.count(unit1) != 0)
-                {
-                    double value = n2 * units_table[element.first][unit1];
-                    units_table[element.first][unit2] = value;
-                    units_table[unit2][element.first] = 1 / value;
-                }
-                //Search unit2 in the value maps and add unit1
-                //And add the founded key to unit1's map
-                else if (element.second.count(unit2) != 0)
-                {
-                    double value = units_table[element.first][unit2] / n2;
-                    units_table[element.first][unit1] = value;
-                    units_table[unit1][element.first] = 1 / value;
-                }
+    double convert(double value, const string& unit1, const string& unit2){
+        if(unit1 == unit2) {return value;} //same type
+        if(units[unit1][unit2] == 0){ //It is impossible to connect them
+            throw invalid_argument{"Unable to convert - ["+unit1+"] to ["+unit2+"]"};
+        }
+        return value*units[unit1][unit2];
+    }
+
+    void NumberWithUnits::read_units(ifstream& file){
+
+        int num1 = 0; 
+        double num2 = 0;
+        string unit1, unit2;
+        char eq = '=';
+        
+        while(file >> num1 >> unit1 >> eq >> num2 >> unit2){
+
+            units[unit1][unit2] = num2;
+            units[unit2][unit1]= num1/num2;
+            
+            for(auto &pair : units[unit1]) {
+                double unitTemp = units[unit2][unit1] * pair.second;
+                units[unit2][pair.first] = unitTemp;
+                units[pair.first][unit2] = 1/unitTemp;            }
+
+            for(auto &pair : units[unit2]) {
+                double unitTemp = units[unit1][unit2] * pair.second;
+                units[unit1][pair.first] = unitTemp;
+                units[pair.first][unit1] = 1/unitTemp;
             }
         }
-    //    // print the map
-    //     for (auto elem : units_table)
-    //     {
-    //         cout <<endl<< "key:" << elem.first <<endl;
-    //         for (auto e2 : elem.second)
-    //         cout << e2.first << " = " << e2.second<<endl;
-    //     }
     }
 
-    double convert_unit(double n, const string& before, const string& after)
-    {
-        if (before == after)
-        {
-            return n;
+
+//input , output
+       ostream& operator<<(ostream& output, const NumberWithUnits& c){
+         return output << c.value << "[" << c.type << "]";
+       }
+
+
+        istream& operator>>(istream& input, NumberWithUnits& c) {
+            double value = 0;
+            string type;
+            char ch = ']';
+            input >> value >> ch;
+            while(ch != ']') {
+                if(ch != '[') { type.insert(type.end(), ch); }
+                input >> ch;
+            }
+
+            if(units[type].empty()){throw invalid_argument{"error unit"};};
+                c.value = value;
+                c.type = type;
+                return input;
+            }
+        
+
+    NumberWithUnits& NumberWithUnits::operator+=(const NumberWithUnits &c) {
+    this->value += convert(c.value, c.type, this->type);
+    return *this;
+    }
+
+    NumberWithUnits& NumberWithUnits::operator-=(const NumberWithUnits& c) {
+    this->value -= convert(c.value, c.type, this->type);
+    return *this;
+    }
+
+    NumberWithUnits NumberWithUnits::operator+(const NumberWithUnits& c) {
+        double cVal = convert(c.value, c.type, this->type);
+        double temp = this->value + cVal;
+        NumberWithUnits ans(temp, this->type);
+        return ans;
+    }
+
+     NumberWithUnits NumberWithUnits::operator-(const NumberWithUnits& c){
+        double cVal = convert(c.value, c.type, this->type);
+        double temp = this->value - cVal;
+        NumberWithUnits ans(temp, this->type);
+        return ans;
+    }
+
+    NumberWithUnits NumberWithUnits::operator+() { //unary
+        NumberWithUnits ans(+this->value , this->type);
+        return ans;
+    }
+
+    NumberWithUnits NumberWithUnits::operator-() { //unary
+        NumberWithUnits ans(-this->value, this->type);
+        return ans;
+    }
+
+    NumberWithUnits& NumberWithUnits::operator++(){//prefix increment  
+            ++(this->value);
+            return *this;
         }
-        if (units_table[after].count(before) != 0)
-        {
-            return n * units_table[before][after];
+
+
+    NumberWithUnits& NumberWithUnits::operator--(){
+            --(this->value);
+            return *this;
         }
-        throw invalid_argument("Not the same unit!");
-    }
 
-    //-------------------------//Six accounting operators-----------------------------
-    NumberWithUnits NumberWithUnits::operator+(const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, this->unit);
-        return NumberWithUnits(this->num + convert, this->unit);
-    }
-
-    NumberWithUnits NumberWithUnits::operator-(const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, this->unit);
-        return NumberWithUnits(this->num - convert, this->unit);
-    }
-
-    NumberWithUnits &NumberWithUnits::operator+=(const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, this->unit);
-        this->num += convert;
-        return *this;
-    }
-
-    NumberWithUnits &NumberWithUnits::operator-=(const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, this->unit);
-        this->num -= convert;
-        return *this;
-    }
-
-    NumberWithUnits NumberWithUnits::operator+()
-    {
-        return NumberWithUnits(+this->num, this->unit);
-    }
-
-    NumberWithUnits NumberWithUnits::operator-()
-    {
-        return NumberWithUnits(-this->num, this->unit);
-    }
-
-    //--------------------------//Six comparison operators---------------------
-    bool operator>(const NumberWithUnits &n1, const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, n1.unit);
-        bool flag = false;
-        if (n1.num > convert)
-        {
-            flag = true;
-            return flag;
+   
+    NumberWithUnits NumberWithUnits::operator++(int dummy_flag_for_postfix_increment){
+            double d = (this->value)++;
+            return  NumberWithUnits(d , this->type);
         }
-        return flag;
-    }
 
-    bool operator>=(const NumberWithUnits &n1, const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, n1.unit);
-        bool flag = false;
+    NumberWithUnits NumberWithUnits::operator--(int dummy_flag_for_postfix_increment){
+            double d = (this->value)--;
+            return  NumberWithUnits(d , this->type);        }
 
-        if (n1.num >= convert || abs(n1.num - convert) < EPSILON)
-        {
-            flag = true;
-            return flag;
-        }
-        return flag;
-    }
 
-    bool operator<(const NumberWithUnits &n1, const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, n1.unit);
-        bool flag = false;
-        if (n1.num < convert)
-        {
-            flag = true;
-            return flag;
-        }
-        return flag;
-    }
+	bool operator==(const NumberWithUnits& c1, const NumberWithUnits& c2){
+        return (abs(c1.value - convert(c2.value, c2.type, c1.type)) <= eps);
+         }
 
-    bool operator<=(const NumberWithUnits &n1, const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, n1.unit);
-        bool flag = false;
-        if (n1.num <= convert || abs(n1.num - convert) < EPSILON)
-        {
-            flag = true;
-            return flag;
-        }
-        return flag;
-    }
+	bool operator!=(const NumberWithUnits& c1, const NumberWithUnits& c2){
+        return !(c1==c2);
+         }
 
-    bool operator==(const NumberWithUnits &n1, const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, n1.unit);
-        bool flag = false;
-        if (n1.num == convert || abs(n1.num - convert) < EPSILON)
-        {
-            flag = true;
-            return flag;
-        }
-        return flag;
-    }
+    bool operator<(const NumberWithUnits& c1, const NumberWithUnits& c2){
+        return ((c1.value - convert(c2.value, c2.type, c1.type)) < 0 );
+         }
 
-    bool operator!=(const NumberWithUnits &n1, const NumberWithUnits &n2)
-    {
-        double convert = convert_unit(n2.num, n2.unit, n1.unit);
-        bool flag = false;
-        if (n1.num != convert && !(abs(n1.num - convert) < EPSILON))
-        {
-            flag = true;
-            return flag;
-        }
-        return flag;
-    }
+	bool operator>(const NumberWithUnits& c1, const NumberWithUnits& c2){
+        return ((c1.value - convert(c2.value, c2.type, c1.type)) >  0);
+         }
 
-    //---------Increase by 1 (++) and decrease by 1 (--) before and after the number--------
-    NumberWithUnits &NumberWithUnits::operator++()
-    {
-        ++this->num;
-        return *this;
-    }
+	bool operator<=(const NumberWithUnits& c1, const NumberWithUnits& c2){
+        return ((c1<c2)|| (c1==c2));
+         }
 
-    NumberWithUnits NumberWithUnits::operator++(int)
-    {
-        return NumberWithUnits((this->num)++, this->unit);
-    }
+	bool operator>=(const NumberWithUnits& c1, const NumberWithUnits& c2){
+        return ((c1>c2)|| (c1==c2));
+         }
 
-    NumberWithUnits &NumberWithUnits::operator--()
-    {
-        --this->num;
-        return *this;
-    }
 
-    NumberWithUnits NumberWithUnits::operator--(int)
-    {
-        return NumberWithUnits((this->num)--, this->unit);
-    }
+	NumberWithUnits operator*(const NumberWithUnits& c1, const double num){
+        return NumberWithUnits(c1.value*num , c1.type);
+         }
 
-    //---------------------Multiplication by real number-------------------
-    NumberWithUnits operator*(const NumberWithUnits &n, double x)
-    {
-        return NumberWithUnits(n.num * x, n.unit);
-    }
-
-    NumberWithUnits operator*(double x, const NumberWithUnits &n)
-    {
-        return NumberWithUnits(n.num * x, n.unit);
-    }
-
-    NumberWithUnits &NumberWithUnits::operator*=(double x)
-    {
-        this->num *= x;
-        return *this;
-    }
-
-    //-------------------------Input and output operator--------------------
-    ostream &operator<<(ostream &output, const NumberWithUnits &n)
-    {
-        output << n.num << "[" << n.unit << "]";
-        return output;
-    }
-
-    istream &operator>>(istream &input, NumberWithUnits &n)
-    {
-        string num;
-        string unit;
-        getline(input, num, '[');
-        getline(input, unit, ']');
-        num.erase(remove(num.begin(), num.end(), ' '), num.end());
-        unit.erase(remove(unit.begin(), unit.end(), ' '), unit.end());
-        if (units_table.count(unit) == 0)
-        {
-            throw invalid_argument{"Unit doesn't exist!!!"};
-        }
-        n.num = stod(num);
-        n.unit = unit;
-        return input;
-    }
-}
+    NumberWithUnits operator*(const double num, const NumberWithUnits& c1 ){
+        return NumberWithUnits(num*c1.value,c1.type);
+         }
+   }
