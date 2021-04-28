382c72e0b0ff4ba3f1640f0fb60df2c3418aa009
shayleyzerovich <58327880+shayleyzerovich@users.noreply.github.com>
2021-04-28 12:42:18 +0300

    Update NumberWithUnits.hpp

--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -26,41 +26,32 @@ class NumberWithUnits{
 
         static void read_units(std::ifstream& f);
 
-        //prefix increment  
+        NumberWithUnits operator+();  
         NumberWithUnits& operator++();
+        NumberWithUnits operator+(const NumberWithUnits& n);
+	NumberWithUnits operator++(int n);
+	NumberWithUnits operator-();
         NumberWithUnits& operator--();
+	NumberWithUnits operator-(const NumberWithUnits& n);
+        NumberWithUnits operator--(int n);
 
-        //postfix increment
-        NumberWithUnits operator++(int dummy_flag_for_postfix_increment);
-        NumberWithUnits operator--(int dummy_flag_for_postfix_increment);
+       
+        NumberWithUnits& operator+=(const NumberWithUnits& n) ;
+        NumberWithUnits& operator-=(const NumberWithUnits& n) ;
+       
+        NumberWithUnits operator*(const NumberWithUnits& n, const double num);
+        NumberWithUnits operator*(const double num, const NumberWithUnits& n);
 
-        //unary
-        NumberWithUnits operator+(); 
-        NumberWithUnits operator-();
-
-        // arithmetic operators
-        NumberWithUnits& operator+=(const NumberWithUnits& c) ;
-        NumberWithUnits& operator-=(const NumberWithUnits& c) ;
-        NumberWithUnits operator+(const NumberWithUnits& c);
-        NumberWithUnits operator-(const NumberWithUnits& c);
-
-        //output , input
-        friend std::ostream& operator<<(std::ostream& output, const NumberWithUnits& c);
-        friend std::istream& operator>>(std::istream& input, NumberWithUnits& c);
-
-        //bool Comparisons
-        bool operator==(const NumberWithUnits& c1, const NumberWithUnits& c2);
-        bool operator!=(const NumberWithUnits& c1, const NumberWithUnits& c2);
-        bool operator<(const NumberWithUnits& c1, const NumberWithUnits& c2);	
-        bool operator>(const NumberWithUnits& c1, const NumberWithUnits& c2);
-        bool operator<=(const NumberWithUnits& c1, const NumberWithUnits& c2);
-        bool operator>=(const NumberWithUnits& c1, const NumberWithUnits& c2);
-
-         //mult
-        NumberWithUnits operator*(const NumberWithUnits& c1, const double num);
-        NumberWithUnits operator*(const double num, const NumberWithUnits& c1);
-
-
+        bool operator==(const NumberWithUnits& n1, const NumberWithUnits& n2);
+        bool operator!=(const NumberWithUnits& n1, const NumberWithUnits& n2);
+        bool operator<(const NumberWithUnits& n1, const NumberWithUnits& n2);	
+        bool operator>(const NumberWithUnits& n1, const NumberWithUnits& n2);
+        bool operator<=(const NumberWithUnits& n1, const NumberWithUnits& n2);
+        bool operator>=(const NumberWithUnits& n1, const NumberWithUnits& n2);
+	
+        friend std::ostream& operator<<(std::ostream& output, const NumberWithUnits& n);
+        friend std::istream& operator>>(std::istream& input, NumberWithUnits& n);
+    
 
   };
 
